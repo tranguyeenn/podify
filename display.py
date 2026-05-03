f93069d40d459b3dc17e098d1d4d030fd7d2929d
Trang Nguyen <tranguyeenn2007@gmail.com>
2026-05-02 22:20:47 -0400

    Add handle search, volume and multiple files for clarity

---
+++ display.py
@@ -0,0 +1,14 @@
+def show_help():
+    print("""
+    Commands:
+        now              Show current song
+        play/p           Resume playback
+        pause/ps         Pause playback
+        next/n           Skip to next track
+        previous/prev    Go to previous track
+        volume 0-100     Set Spotify volume
+        macvol 0-100     Set Mac system volume
+        search <query>   Search Spotify for a track, album, or artist
+        help/h           Show this menu
+        quit/q/ exit     Exit
+""")
--- podify.py
+++ podify.py
@@ -1,75 +1,44 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-from dotenv import load_dotenv
-from spotipy.exceptions import SpotifyException
-from colorama import init, Fore, Style
+from colorama import init
+
+from config import sp
+from commands import current, pause, next_track, previous, play, handle_volume, handle_search
+from mac_controls import handle_mac_volume
+from display import show_help
 
 init(autoreset=True)
 
-load_dotenv()
-
-scope = "user-read-playback-state user-modify-playback-state user-read-currently-playing"
-sp = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(
-        scope=scope,
-        redirect_uri="http://127.0.0.1:8888/callback"
-    )
-)
-
-def current():
-    current_track = sp.current_user_playing_track()
-    
-    if current_track is not None:
-        track_name = current_track['item']['name']
-        artists = ', '.join([artist['name'] for artist in current_track['item']['artists']])
-        print(Style.BRIGHT + Fore.GREEN + f"🎧 Currently playing: {track_name} by {artists}")
-
-    else:
-        print("No track is currently playing.")
-
-def pause():
-    sp.pause_playback()
-    print("⏸ paused.")
-
-def next():
-    sp.next_track()
-    print("⏭ skipped.")
-
-
-def previous():
-    try:
-        sp.previous_track()
-        print("⏮ previous track")
-    except SpotifyException as e:
-        if e.http_status == 403:
-            print()
-            print(Fore.RED + "Spotify blocked previous track for this playback.")
-            print(Fore.RED+ "Try playing from a normal playlist/album, not queue/radio/podcast/ad.")
-        else:
-            print(Fore.RED + "Spotify error:", e)
-
-def play():
-    sp.start_playback()
-    print("🎧 Playback started.")
-
-def exit():
-    print("Exiting...")
-    quit()
+print("Type 'help' to see available commands.")
 
 while True:
-    cmd = input("spotify> ").strip().lower()
+    user_input = input("spotify> ").strip()
 
-    if cmd == "now":
-        current()
-    elif cmd == "pause":
-        pause()
-    elif cmd == "next":
-        next()
-    elif cmd == "previous":
-        previous()
-    elif cmd == "play":
-        play()
-    elif cmd == "exit":
-        exit()
+    if not user_input:
+        continue
+
+    parts = user_input.split()
+    cmd = parts[0].lower()
+    args = parts[1:]
+
+    if cmd in ["help", "h"]:
+        show_help()
+    elif cmd == "now":
+        current(sp)
+    elif cmd in ["pause", "ps"]:
+        pause(sp)
+    elif cmd in ["next", "n"]:
+        next_track(sp)
+    elif cmd in ["previous", "prev"]:
+        previous(sp)
+    elif cmd in ["play", "p"]:
+        play(sp)
+    elif cmd == "volume":
+        handle_volume(sp, args)
+    elif cmd == "macvol":
+        handle_mac_volume(args)
+    elif cmd == "search":
+        handle_search(sp, args)
+    elif cmd in ["exit", "quit", "q"]:
+        print("Exiting...")
+        break
     else:
-        print("Commands: now, pause, next, previous, play, exit")
+        print("Unknown command. Type 'help'.")
